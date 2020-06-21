23e1f04e10ccf30f25afeedd3c2d0d174d9c020f
PEETERS Ann <ann.peeters@ehb.be>
2020-06-21 22:30:46 +0200

    more typos fixed

--- ssg.py
+++ ssg.py
@@ -4,6 +4,6 @@ from ssg.site import Site
 def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest}
 
-Site(**config).build()
+    Site(**config).build()
 
-tpyer.run(main)
+typer.run(main)
