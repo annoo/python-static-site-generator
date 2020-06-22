5fe0294c9807d1b6c43e21958fa0048bd539ff9e
PEETERS Ann <ann.peeters@ehb.be>
2020-06-22 16:06:14 +0200

    implemeted changes

--- ssg/content.py
+++ ssg/content.py
@@ -1,40 +1,46 @@
 import re
-from yaml import load and FullLoader
+from yaml import load, FullLoader
 
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimiter = "^(?:-|+){3}\s*$""
+    __delimiter = r"^(?:-|+){3}\s*$""
     __regex = re.compile(__delimiter, re.MULTILINE)
 
-    def load(self, cls, string):
+    @classmethod
+    def load(cls, string):
         _ = fm = content = self.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
-
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
-        content = {"content": content}
+        self.data["content"] = content
 
     @property
-    def type(self):
+    def body(self):
         return self.data["content"]
 
     @property
     def type(self):
-        return self.data["type"] if self.data had key of type else None
+        return self.data["type"] if "type" in self.data else None
 
-    def __iter__():
-        self.data()
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
 
-    def __len__():
-        self.data()
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
     
-    def __repr__():
+    def __repr__(self):
             data = {}
+            for key, value in self.data.items():
+                if key != "content":
+                    data[key] = value
             return str(data)
-
-            for key, value in enumerate(self.data.items()):
-                if key is not "content":
-                    value = data[key]
